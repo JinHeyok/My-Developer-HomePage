267a4669278b5e2aad9c889bfa3eed57a6dc51f7
JinHeyok <qwerkpjh@naver.com>
2023-11-18 20:27:28 +0900

    email sned updated

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -6,75 +6,54 @@
 (function () {
   "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+  let forms = document.getElementById('gform');
 
-  forms.forEach( function(e) {
-    e.addEventListener('submit', function(event) {
-      event.preventDefault();
+  forms.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-      let thisForm = this;
+    let thisForm = this;
 
-      let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!');
-        return;
-      }
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.remove('d-block');
+    let action = thisForm.getAttribute('action');
+    
+    if( ! action ) {
+      displayError(thisForm, 'The form action property is not set!');
+      return;
+    }
 
-      let formData = new FormData( thisForm );
+    thisForm.querySelector('.loading').classList.add('d-block');
+    thisForm.querySelector('.error-message').classList.remove('d-block');
+    thisForm.querySelector('.sent-message').classList.remove('d-block');
+    
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let subject = document.getElementById("subject");
+    let message = document.getElementById("message");
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error);
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+    let formData = new FormData;
+    const request = new XMLHttpRequest;
+  
+    formData.append("name" , name.value);
+    formData.append("email" , email.value);
+    formData.append("subject" , subject.value);
+    formData.append("message" , message.value);
+
+    request.open("POST" ,"https://script.google.com/macros/s/AKfycbyFsS0JLSsCFaY2BCP5jGp-N86_fovtepC69HZdF9F74t0-y8csVd2x8OCdGsOWx8mz/exec")
+    request.addEventListener("readystatechange" , function name(params) {
+      if(request.readyState == XMLHttpRequest.DONE){
+        if(request.status === 200){
+          const response = JSON.parse(request.response);
+          if(response.result == "success"){
+            thisForm.querySelector('.loading').classList.remove('d-block');
+            thisForm.querySelector('.sent-message').classList.add('d-block');
+          } else {
+            console.error(response);
+            displayError(thisForm , "메일 전송에 실패했습니다.");
+          }
         }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
       }
-    });
+    })
+    request.send(formData);
   });
-
-  function php_email_form_submit(thisForm, action, formData) {
-    fetch(action, {
-      method: 'POST',
-      body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
-    })
-    .then(response => {
-      if( response.ok ) {
-        return response.text();
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
-  }
 
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
